d6c2b8550b06f1fff0f41307016d52ea5b24b5ee
oyzh <oyzh.1994@qq.com>
2024-04-02 18:24:35 +0800

    1. 部分细节处理

--- src/main/java/cn/oyzh/easyzk/fx/ZKRichDataTextArea.java
+++ src/main/java/cn/oyzh/easyzk/fx/ZKRichDataTextArea.java
@@ -233,7 +233,6 @@ public class ZKRichDataTextArea extends FlexRichTextArea {
             } else {
                 super.changeTheme(ThemeManager.currentTheme());
             }
-            this.forgetHistory();
         });
     }
 

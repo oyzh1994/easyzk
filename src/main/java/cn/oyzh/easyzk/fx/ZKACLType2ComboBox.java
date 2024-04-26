323341cf4ff518143f6c0157ed7ec91a8c877e29
oyzh <oyzh.1994@qq.com>
2024-04-26 18:56:34 +0800

    1. i18n完善

---
+++ src/main/java/cn/oyzh/easyzk/fx/ZKACLType2ComboBox.java
@@ -0,0 +1,32 @@
+package cn.oyzh.easyzk.fx;
+
+import cn.oyzh.fx.plus.controls.combo.FlexComboBox;
+import cn.oyzh.fx.plus.i18n.I18nSelectAdapter;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * @author oyzh
+ * @since 2024/4/26
+ */
+public class ZKACLType2ComboBox extends FlexComboBox<String> implements I18nSelectAdapter<String> {
+
+    @Override
+    public List<String> values(Locale locale) {
+        this.clearItems();
+        if (locale == Locale.SIMPLIFIED_CHINESE) {
+            this.addItem("开放认证(WORLD)");
+            this.addItem("摘要认证(DIGEST)");
+            this.addItem("IP认证(IP)");
+        } else if (locale == Locale.TRADITIONAL_CHINESE) {
+            this.addItem("開放認證(WORLD)");
+            this.addItem("IP認證(IP)");
+        } else if (locale == Locale.ENGLISH) {
+            this.addItem("WORLD");
+            this.addItem("DIGEST");
+            this.addItem("IP");
+        }
+        return this.getItems();
+    }
+}

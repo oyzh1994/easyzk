34bf120b5485e6f622ef9ea1b42095f686adc759
oyzh <oyzh.1994@qq.com>
2024-06-18 14:48:27 +0800

    1. 打包功能完善

--- src/test/java/cn/oyzh/easyzk/test/PackTest.java
+++ src/test/java/cn/oyzh/easyzk/test/PackTest.java
@@ -23,15 +23,15 @@ public class PackTest {
     public void pkg_easyzk_win() throws Exception {
 
         String packagePath = this.getPackagePath();
-        String win_jre_config = packagePath + "win_jre_config.json";
-        String win_jar_config = packagePath + "win_jar_config.json";
+        // String win_jre_config = packagePath + "win_jre_config.json";
+        // String win_jar_config = packagePath + "win_jar_config.json";
         String win_pack_config = packagePath + "win_pack_config.json";
-        String win_jlink_config = packagePath + "win_jlink_config.json";
+        // String win_jlink_config = packagePath + "win_jlink_config.json";
 
         Packer packer = new Packer();
-        packer.registerJreHandler(win_jre_config);
-        packer.registerJarHandler(win_jar_config);
-        packer.registerJLinkHandler(win_jlink_config);
+        // packer.registerJreHandler(win_jre_config);
+        // packer.registerJarHandler(win_jar_config);
+        // packer.registerJLinkHandler(win_jlink_config);
         packer.pack(win_pack_config);
     }
 
@@ -39,13 +39,13 @@ public class PackTest {
     public void pkg_easyzk_linux() throws Exception {
 
         String packagePath = this.getPackagePath();
-        String linux_jre_config = packagePath + "linux_jre_config.json";
-        String linux_jar_config = packagePath + "linux_jar_config.json";
+        // String linux_jre_config = packagePath + "linux_jre_config.json";
+        // String linux_jar_config = packagePath + "linux_jar_config.json";
         String linux_pack_config = packagePath + "linux_pack_config.json";
 
         Packer packer = new Packer();
-        packer.registerJreHandler(linux_jre_config);
-        packer.registerJarHandler(linux_jar_config);
+        // packer.registerJreHandler(linux_jre_config);
+        // packer.registerJarHandler(linux_jar_config);
         packer.pack(linux_pack_config);
     }
 
@@ -53,13 +53,13 @@ public class PackTest {
     public void pkg_easyzk_macos() throws Exception {
 
         String packagePath = this.getPackagePath();
-        String macos_jre_config = packagePath + "macos_jre_config.json";
-        String macos_jar_config = packagePath + "macos_jar_config.json";
+        // String macos_jre_config = packagePath + "macos_jre_config.json";
+        // String macos_jar_config = packagePath + "macos_jar_config.json";
         String macos_pack_config = packagePath + "macos_pack_config.json";
 
         Packer packer = new Packer();
-        packer.registerJreHandler(macos_jre_config);
-        packer.registerJarHandler(macos_jar_config);
+        // packer.registerJreHandler(macos_jre_config);
+        // packer.registerJarHandler(macos_jar_config);
         packer.pack(macos_pack_config);
     }
 

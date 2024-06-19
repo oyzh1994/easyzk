107b097ac68e74d47d5680b0290f5d0209d68549
oyzh <oyzh.1994@qq.com>
2024-06-19 18:37:54 +0800

    1. 打包系统完善，支持mvn预安装和打包

--- src/test/java/cn/oyzh/easyzk/test/AppTest.java
+++ src/test/java/cn/oyzh/easyzk/test/AppTest.java
@@ -1,14 +1,35 @@
 package cn.oyzh.easyzk.test;
 
+import com.sun.tools.javac.Main;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 /**
  * @author oyzh
  * @since 2022/5/18
  */
 public class AppTest {
 
-    public static void main(String[] args) {
-        TerminalApp.main(args);
+    public static void main(String[] args) throws URISyntaxException {
+        // TerminalApp.main(args);
 //        AppMain.main(args);
+//         URL rootUrl = AppTest.class.getProtectionDomain().getCodeSource().getLocation();
+//         File rootDir = new File(rootUrl.getPath());
+//         System.out.println(rootDir);
+
+        // 获取名为"PATH"的环境变量
+        String path = System.getenv("PATH");
+        System.out.println("PATH环境变量的值: " + path);
+
+        // 你可以获取任何其他环境变量的值，只需要替换环境变量的名称即可
+        String mavenHome = System.getenv("MAVEN_HOME"); // 假设你设置了一个名为MAVEN_HOME的环境变量
+        if (mavenHome != null) {
+            System.out.println("MAVEN_HOME环境变量的值: " + mavenHome);
+        } else {
+            System.out.println("MAVEN_HOME环境变量未设置");
+        }
     }
 
 }
--- src/test/java/cn/oyzh/easyzk/test/PackTest.java
+++ src/test/java/cn/oyzh/easyzk/test/PackTest.java
@@ -3,6 +3,8 @@ package cn.oyzh.easyzk.test;
 import cn.oyzh.fx.pkg.Packer;
 import org.junit.Test;
 
+import java.util.List;
+
 /**
  * @author oyzh
  * @since 2023/3/8
@@ -57,6 +59,26 @@ public class PackTest {
         Packer packer = new Packer();
         packer.registerProjectHandler();
         packer.pack(macos_pack_config);
+    }
+
+    @Test
+    public void easyzk_all_pack() throws Exception {
+        String packagePath = this.getPackagePath();
+        String win_amd64_pack_config = packagePath + "win_amd64_pack_config.json";
+        String linux_amd64_pack_config = packagePath + "linux_amd64_pack_config.json";
+        String linux_arm64_pack_config = packagePath + "linux_arm64_pack_config.json";
+        String macos_amd64_pack_config = packagePath + "macos_amd64_pack_config.json";
+
+        String baseDir = "D:\\Workspaces\\OYZH\\fx-base\\";
+        String projectDir = "D:\\Workspaces\\OYZH\\easyzk\\";
+
+        Packer packer = new Packer();
+        packer.registerProjectHandler();
+        packer.registerMvnHandler(projectDir, List.of(baseDir));
+        packer.pack(win_amd64_pack_config);
+        packer.pack(linux_amd64_pack_config);
+        packer.pack(linux_arm64_pack_config);
+        packer.pack(macos_amd64_pack_config);
     }
 
 }

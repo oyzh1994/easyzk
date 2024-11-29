7ab9879a358873231cb84db00645d1be49338204
oyzh <oyzh.1994@qq.com>
2024-11-29 11:35:59 +0800

    1. 新增四字命令kill、ruok

---
+++ src/main/java/cn/oyzh/easyzk/terminal/fourlw/ZKKillCommand.java
@@ -0,0 +1,12 @@
+package cn.oyzh.easyzk.terminal.fourlw;
+
+/**
+ * @author oyzh
+ * @since 2024-11-29
+ */
+public class ZKKillCommand extends ZKFourLetterWordCommand {
+
+    public ZKKillCommand() {
+        super("kill");
+    }
+}

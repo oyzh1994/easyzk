1605d0cf716c90d3b874ba5a959fcb5c9644b305
oyzh <oyzh.1994@qq.com>
2024-11-29 11:38:27 +0800

    1. 新增四字命令wchs、wchc、wchp

---
+++ src/main/java/cn/oyzh/easyzk/terminal/fourlw/ZKWchpCommandHandler.java
@@ -0,0 +1,23 @@
+package cn.oyzh.easyzk.terminal.fourlw;
+
+import cn.oyzh.fx.plus.i18n.I18nResourceBundle;
+import cn.oyzh.fx.terminal.command.TerminalCommand;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+/**
+ * @author oyzh
+ * @since 2023/09/20
+ */
+public class ZKWchpCommandHandler extends ZKFourLetterWordCommandHandler<TerminalCommand> {
+
+    @Getter(AccessLevel.PROTECTED)
+    @Accessors(fluent = true)
+    private final ZKFourLetterWordCommand furLetterWordCommand = new ZKWchpCommand();
+
+    @Override
+    public String commandDesc() {
+        return I18nResourceBundle.i18nString("base.add", "base.auth");
+    }
+}

e93c068b643d7310962fbece3c1c6341a469a2ea
oyzh <oyzh.1994@qq.com>
2024-12-18 14:59:09 +0800

    1. 细节处理

--- src/main/java/cn/oyzh/easyzk/controller/HeaderController.java
+++ src/main/java/cn/oyzh/easyzk/controller/HeaderController.java
@@ -1,115 +1,115 @@
-package cn.oyzh.easyzk.controller;
-
-import cn.oyzh.common.SysConst;
-import cn.oyzh.easyzk.controller.data.ZKDataTransportController;
-import cn.oyzh.easyzk.controller.data.ZKDataMigrationController;
-import cn.oyzh.easyzk.controller.tool.ZKToolController;
-import cn.oyzh.easyzk.event.ZKEventUtil;
-import cn.oyzh.fx.plus.controller.SubStageController;
-import cn.oyzh.fx.plus.information.MessageBox;
-import cn.oyzh.fx.plus.window.StageAdapter;
-import cn.oyzh.fx.plus.window.StageManager;
-import cn.oyzh.i18n.I18nHelper;
-import javafx.fxml.FXML;
-
-/**
- * 主页头部业务
- *
- * @author oyzh
- * @since 2022/1/26
- */
-@Deprecated
-public class HeaderController extends SubStageController {
-
-    /**
-     * 认证管理
-     */
-    @FXML
-    private void auth() {
-        ZKEventUtil.authMain();
-    }
-
-    /**
-     * 设置
-     */
-    @FXML
-    private void setting() {
-        StageAdapter wrapper = StageManager.getStage(SettingController.class);
-        if (wrapper != null) {
-            wrapper.toFront();
-        } else {
-            StageManager.showStage(SettingController.class, this.stage);
-        }
-    }
-
-    /**
-     * 关于
-     */
-    @FXML
-    private void about() {
-        StageManager.showStage(AboutController.class, this.stage);
-    }
-
-    /**
-     * 过滤
-     */
-    @FXML
-    private void filter() {
-        ZKEventUtil.filterMain();
-    }
-
-    /**
-     * 退出
-     */
-    @FXML
-    private void quit() {
-        if (MessageBox.confirm(I18nHelper.quit() + " " + SysConst.projectName())) {
-            StageManager.exit();
-        }
-    }
-
-    /**
-     * 传输数据
-     */
-    @FXML
-    private void transport() {
-        StageAdapter wrapper = StageManager.getStage(ZKDataTransportController.class);
-        if (wrapper != null) {
-            wrapper.toFront();
-        } else {
-            StageManager.showStage(ZKDataTransportController.class);
-        }
-    }
-
-    /**
-     * 工具箱
-     */
-    @FXML
-    private void tool() {
-        StageManager.showStage(ZKToolController.class, StageManager.getPrimaryStage());
-    }
-
-    /**
-     * 布局1
-     */
-    @FXML
-    private void layout1() {
-        ZKEventUtil.layout1();
-    }
-
-    /**
-     * 布局2
-     */
-    @FXML
-    private void layout2() {
-        ZKEventUtil.layout2();
-    }
-
-    /**
-     * 迁移
-     */
-    @FXML
-    private void migration() {
-        StageManager.showStage(ZKDataMigrationController.class);
-    }
-}
+// package cn.oyzh.easyzk.controller;
+//
+// import cn.oyzh.common.SysConst;
+// import cn.oyzh.easyzk.controller.data.ZKDataTransportController;
+// import cn.oyzh.easyzk.controller.data.ZKDataMigrationController;
+// import cn.oyzh.easyzk.controller.tool.ZKToolController;
+// import cn.oyzh.easyzk.event.ZKEventUtil;
+// import cn.oyzh.fx.plus.controller.SubStageController;
+// import cn.oyzh.fx.plus.information.MessageBox;
+// import cn.oyzh.fx.plus.window.StageAdapter;
+// import cn.oyzh.fx.plus.window.StageManager;
+// import cn.oyzh.i18n.I18nHelper;
+// import javafx.fxml.FXML;
+//
+// /**
+//  * 主页头部业务
+//  *
+//  * @author oyzh
+//  * @since 2022/1/26
+//  */
+// @Deprecated
+// public class HeaderController extends SubStageController {
+//
+//     /**
+//      * 认证管理
+//      */
+//     @FXML
+//     private void auth() {
+//         ZKEventUtil.authMain();
+//     }
+//
+//     /**
+//      * 设置
+//      */
+//     @FXML
+//     private void setting() {
+//         StageAdapter wrapper = StageManager.getStage(SettingController.class);
+//         if (wrapper != null) {
+//             wrapper.toFront();
+//         } else {
+//             StageManager.showStage(SettingController.class, this.stage);
+//         }
+//     }
+//
+//     /**
+//      * 关于
+//      */
+//     @FXML
+//     private void about() {
+//         StageManager.showStage(AboutController.class, this.stage);
+//     }
+//
+//     /**
+//      * 过滤
+//      */
+//     @FXML
+//     private void filter() {
+//         ZKEventUtil.filterMain();
+//     }
+//
+//     /**
+//      * 退出
+//      */
+//     @FXML
+//     private void quit() {
+//         if (MessageBox.confirm(I18nHelper.quit() + " " + SysConst.projectName())) {
+//             StageManager.exit();
+//         }
+//     }
+//
+//     /**
+//      * 传输数据
+//      */
+//     @FXML
+//     private void transport() {
+//         StageAdapter wrapper = StageManager.getStage(ZKDataTransportController.class);
+//         if (wrapper != null) {
+//             wrapper.toFront();
+//         } else {
+//             StageManager.showStage(ZKDataTransportController.class);
+//         }
+//     }
+//
+//     /**
+//      * 工具箱
+//      */
+//     @FXML
+//     private void tool() {
+//         StageManager.showStage(ZKToolController.class, StageManager.getPrimaryStage());
+//     }
+//
+//     /**
+//      * 布局1
+//      */
+//     @FXML
+//     private void layout1() {
+//         ZKEventUtil.layout1();
+//     }
+//
+//     /**
+//      * 布局2
+//      */
+//     @FXML
+//     private void layout2() {
+//         ZKEventUtil.layout2();
+//     }
+//
+//     /**
+//      * 迁移
+//      */
+//     @FXML
+//     private void migration() {
+//         StageManager.showStage(ZKDataMigrationController.class);
+//     }
+// }

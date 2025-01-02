a32290c253dc675b0486ca384bb600aea819c8f3
oyzh <oyzh.1994@q.qcom>
2025-01-02 17:54:42 +0800

    1. 修复因为文本边界检查导致的样式配色失效问题

--- src/main/java/cn/oyzh/easyzk/fx/ZKDataTextArea.java
+++ src/main/java/cn/oyzh/easyzk/fx/ZKDataTextArea.java
@@ -2,9 +2,23 @@ package cn.oyzh.easyzk.fx;
 
 import cn.oyzh.easyzk.domain.ZKSetting;
 import cn.oyzh.easyzk.store.ZKSettingStore;
+import cn.oyzh.fx.rich.richtextfx.data.RichDataTextArea;
 import cn.oyzh.fx.rich.richtextfx.data.RichDataTextAreaPane;
+import cn.oyzh.fx.rich.richtextfx.data.RichDataType;
 
 public class ZKDataTextArea extends RichDataTextAreaPane {
+
+    @Override
+    protected void initTextArea() {
+        RichDataTextArea textArea = super.getContent();
+        // 200k
+        textArea.setStyleBound(RichDataType.HEX, 200 * 1024 * 1024);
+        // 500k
+        textArea.setStyleBound(RichDataType.JSON, 500 * 1024 * 1024);
+        // 100k
+        textArea.setStyleBound(RichDataType.BINARY, 100 * 1024 * 1024);
+        super.initTextArea();
+    }
 
     @Override
     protected void initFont() {

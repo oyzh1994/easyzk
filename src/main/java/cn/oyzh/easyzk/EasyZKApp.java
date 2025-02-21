40baec627a3cd3e5d63b06a887f76bb08ddcce1b
oyzh <oyzh.1994@q.qcom>
2025-02-21 15:07:45 +0800

    1. 页面显示事件驱动改造

--- src/main/java/cn/oyzh/easyzk/EasyZKApp.java
+++ src/main/java/cn/oyzh/easyzk/EasyZKApp.java
@@ -3,15 +3,47 @@ package cn.oyzh.easyzk;
 import cn.oyzh.common.SysConst;
 import cn.oyzh.common.dto.Project;
 import cn.oyzh.common.log.JulLog;
+import cn.oyzh.easyzk.controller.AboutController;
 import cn.oyzh.easyzk.controller.MainController;
+import cn.oyzh.easyzk.controller.SettingController2;
+import cn.oyzh.easyzk.controller.acl.ZKACLAddController;
+import cn.oyzh.easyzk.controller.acl.ZKACLUpdateController;
+import cn.oyzh.easyzk.controller.connect.ZKAddConnectController;
+import cn.oyzh.easyzk.controller.connect.ZKUpdateConnectController;
+import cn.oyzh.easyzk.controller.data.ZKExportDataController;
+import cn.oyzh.easyzk.controller.data.ZKImportDataController;
+import cn.oyzh.easyzk.controller.data.ZKMigrationDataController;
+import cn.oyzh.easyzk.controller.data.ZKMigrationTipsController;
+import cn.oyzh.easyzk.controller.data.ZKTransportDataController;
+import cn.oyzh.easyzk.controller.node.ZKAddNodeController;
+import cn.oyzh.easyzk.controller.node.ZKAuthNodeController;
+import cn.oyzh.easyzk.controller.node.ZKQRCodeNodeController;
+import cn.oyzh.easyzk.controller.tool.ZKToolController;
 import cn.oyzh.easyzk.domain.ZKSetting;
 import cn.oyzh.easyzk.event.ZKEventUtil;
+import cn.oyzh.easyzk.event.window.ZKShowAboutEvent;
+import cn.oyzh.easyzk.event.window.ZKShowAddACLEvent;
+import cn.oyzh.easyzk.event.window.ZKShowAddConnectEvent;
+import cn.oyzh.easyzk.event.window.ZKShowAddNodeEvent;
+import cn.oyzh.easyzk.event.window.ZKShowAuthNodeEvent;
+import cn.oyzh.easyzk.event.window.ZKShowExportDataEvent;
+import cn.oyzh.easyzk.event.window.ZKShowImportDataEvent;
+import cn.oyzh.easyzk.event.window.ZKShowMainEvent;
+import cn.oyzh.easyzk.event.window.ZKShowMigrationDataEvent;
+import cn.oyzh.easyzk.event.window.ZKShowQRCodeNodeEvent;
+import cn.oyzh.easyzk.event.window.ZKShowSettingEvent;
+import cn.oyzh.easyzk.event.window.ZKShowToolEvent;
+import cn.oyzh.easyzk.event.window.ZKShowTransportDataEvent;
+import cn.oyzh.easyzk.event.window.ZKShowUpdateACLEvent;
+import cn.oyzh.easyzk.event.window.ZKShowUpdateConnectEvent;
 import cn.oyzh.easyzk.exception.ZKExceptionParser;
 import cn.oyzh.easyzk.store.ZKSettingStore;
 import cn.oyzh.easyzk.store.ZKStoreUtil;
 import cn.oyzh.easyzk.terminal.ZKTerminalManager;
 import cn.oyzh.easyzk.zk.ZKSASLUtil;
 import cn.oyzh.event.EventFactory;
+import cn.oyzh.event.EventListener;
+import cn.oyzh.event.EventSubscribe;
 import cn.oyzh.fx.gui.tray.DesktopTrayItem;
 import cn.oyzh.fx.gui.tray.QuitTrayItem;
 import cn.oyzh.fx.gui.tray.SettingTrayItem;
@@ -28,6 +60,7 @@ import cn.oyzh.fx.plus.util.FXUtil;
 import cn.oyzh.fx.plus.window.StageAdapter;
 import cn.oyzh.fx.plus.window.StageManager;
 import cn.oyzh.fx.terminal.util.TerminalManager;
+import cn.oyzh.i18n.I18nHelper;
 import cn.oyzh.i18n.I18nManager;
 import javafx.stage.Stage;
 
@@ -40,7 +73,7 @@ import java.awt.event.MouseEvent;
  * @author oyzh
  * @since 2020/9/14
  */
-public class EasyZKApp extends FXApplication {
+public class EasyZKApp extends FXApplication implements EventListener {
 
     /**
      * 项目信息
@@ -97,6 +130,8 @@ public class EasyZKApp extends FXApplication {
             OpacityManager.apply(setting.opacityConfig());
             // 注册异常处理器
             MessageBox.registerExceptionParser(ZKExceptionParser.INSTANCE);
+            // 注册事件处理
+            EventListener.super.register();
             // 调用父类
             super.init();
         } catch (Exception ex) {
@@ -123,14 +158,21 @@ public class EasyZKApp extends FXApplication {
     }
 
     @Override
+    public void stop() {
+        super.stop();
+        EventListener.super.unregister();
+    }
+
+    @Override
     protected void showMainView() {
-        try {
-            // 显示主页面
-            StageManager.showStage(MainController.class);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            JulLog.warn("showMainView error", ex);
-        }
+//        try {
+//            // 显示主页面
+//            StageManager.showStage(MainController.class);
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//            JulLog.warn("showMainView error", ex);
+//        }
+        this.showMain(null);
     }
 
     @Override
@@ -148,11 +190,9 @@ public class EasyZKApp extends FXApplication {
             // 设置标题
             TrayManager.setTitle(PROJECT.getName() + " v" + PROJECT.getVersion());
             // 打开主页
-            TrayManager.addMenuItem(new DesktopTrayItem("12", ZKEventUtil::showMain));
-//            TrayManager.addMenuItem(new DesktopTrayItem("12", this::showMain));
+            TrayManager.addMenuItem(new DesktopTrayItem("12", () -> this.showMain(null)));
             // 打开设置
-//            TrayManager.addMenuItem(new SettingTrayItem("12", this::showSetting));
-            TrayManager.addMenuItem(new SettingTrayItem("12", ZKEventUtil::showSetting));
+            TrayManager.addMenuItem(new SettingTrayItem("12", () -> this.showSetting(null)));
             // 退出程序
             TrayManager.addMenuItem(new QuitTrayItem("12", () -> {
                 JulLog.warn("exit app by tray.");
@@ -162,8 +202,7 @@ public class EasyZKApp extends FXApplication {
             TrayManager.onMouseClicked(e -> {
                 // 单击鼠标主键，显示主页
                 if (e.getButton() == MouseEvent.BUTTON1) {
-//                    this.showMain();
-                    ZKEventUtil.showMain();
+                    this.showMain(null);
                 }
             });
             // 显示托盘
@@ -173,35 +212,287 @@ public class EasyZKApp extends FXApplication {
         }
     }
 
-//    /**
-//     * 显示主页
-//     */
-//    private void showMain() {
-//        FXUtil.runLater(() -> {
-//            StageAdapter wrapper = StageManager.getStage(MainController.class);
-//            if (wrapper != null) {
-//                JulLog.info("front main.");
-//                wrapper.toFront();
-//            } else {
-//                JulLog.info("show main.");
-//                StageManager.showStage(MainController.class);
-//            }
-//        });
-//    }
-//
-//    /**
-//     * 显示设置
-//     */
-//    private void showSetting() {
-//        FXUtil.runLater(() -> {
-//            StageAdapter wrapper = StageManager.getStage(SettingController2.class);
-//            if (wrapper != null) {
-//                JulLog.info("front setting.");
-//                wrapper.toFront();
-//            } else {
-//                JulLog.info("show setting.");
-//                StageManager.showStage(SettingController2.class, StageManager.getPrimaryStage());
-//            }
-//        });
-//    }
+    /**
+     * 显示主页
+     */
+    @EventSubscribe
+    private void showMain(ZKShowMainEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+                StageAdapter adapter = StageManager.getStage(MainController.class);
+                if (adapter != null) {
+                    JulLog.info("front main.");
+                    adapter.toFront();
+                } else {
+                    JulLog.info("show main.");
+                    StageManager.showStage(MainController.class);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 显示设置
+     */
+    @EventSubscribe
+    private void showSetting(ZKShowSettingEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+
+                StageAdapter adapter = StageManager.getStage(SettingController2.class);
+                if (adapter != null) {
+                    JulLog.info("front setting.");
+                    adapter.toFront();
+                } else {
+                    JulLog.info("show setting.");
+                    StageManager.showStage(SettingController2.class, StageManager.getPrimaryStage());
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 显示传输数据
+     */
+    @EventSubscribe
+    private void transportData(ZKShowTransportDataEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+
+                StageAdapter adapter = StageManager.parseStage(ZKTransportDataController.class);
+                adapter.setProp("sourceInfo", event.data());
+                adapter.display();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 显示导出数据
+     */
+    @EventSubscribe
+    private void exportData(ZKShowExportDataEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+
+                StageAdapter adapter = StageManager.parseStage(ZKExportDataController.class);
+                adapter.setProp("connect", event.data());
+                adapter.setProp("nodePath", event.path());
+                adapter.display();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 显示导入数据
+     */
+    @EventSubscribe
+    private void importData(ZKShowImportDataEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+
+                StageAdapter adapter = StageManager.parseStage(ZKImportDataController.class);
+                adapter.setProp("connect", event.data());
+                adapter.display();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 显示添加连接
+     */
+    @EventSubscribe
+    private void addConnect(ZKShowAddConnectEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+                StageAdapter adapter = StageManager.parseStage(ZKAddConnectController.class);
+                adapter.setProp("group", event.data());
+                adapter.display();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 显示修改连接
+     */
+    @EventSubscribe
+    private void updateConnect(ZKShowUpdateConnectEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+                StageAdapter adapter = StageManager.parseStage(ZKUpdateConnectController.class);
+                adapter.setProp("zkConnect", event.data());
+                adapter.display();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 添加zk子节点
+     */
+    @EventSubscribe
+    private void addNode(ZKShowAddNodeEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+                StageAdapter adapter = StageManager.parseStage(ZKAddNodeController.class);
+                adapter.setProp("zkItem", event.data());
+                adapter.setProp("zkClient", event.client());
+                adapter.display();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 显示认证节点页面
+     */
+    @EventSubscribe
+    private void authNode(ZKShowAuthNodeEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+                StageAdapter adapter = StageManager.parseStage(ZKAuthNodeController.class);
+                adapter.setProp("zkItem", event.data());
+                adapter.setProp("zkClient", event.client());
+                adapter.display();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 显示节点二维码页面
+     */
+    @EventSubscribe
+    private void qrCodeNode(ZKShowQRCodeNodeEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+                StageAdapter fxView = StageManager.parseStage(ZKQRCodeNodeController.class);
+                fxView.setProp("zkNode", event.data());
+                fxView.setProp("nodeData", event.text());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 显示工具页面
+     */
+    @EventSubscribe
+    private void tool(ZKShowToolEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+                StageManager.showStage(ZKToolController.class, StageManager.getPrimaryStage());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 添加权限
+     */
+    @EventSubscribe
+    private void addACL(ZKShowAddACLEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+                StageAdapter fxView = StageManager.parseStage(ZKACLAddController.class);
+                fxView.setProp("zkItem", event.data());
+                fxView.setProp("zkClient", event.client());
+                fxView.display();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 显示修改权限页面
+     */
+    @EventSubscribe
+    private void updateACL(ZKShowUpdateACLEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+                StageAdapter fxView = StageManager.parseStage(ZKACLUpdateController.class);
+                fxView.setProp("acl", event.acl());
+                fxView.setProp("zkItem", event.data());
+                fxView.setProp("zkClient", event.client());
+                fxView.display();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 显示关于页面
+     */
+    @EventSubscribe
+    private void about(ZKShowAboutEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+                StageManager.showStage(AboutController.class, StageManager.getPrimaryStage());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 显示迁移数据页面
+     */
+    @EventSubscribe
+    private void migrationData(ZKShowMigrationDataEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+                StageManager.showStage(ZKMigrationDataController.class, StageManager.getPrimaryStage());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
+
+    /**
+     * 显示迁移提示页面
+     */
+    @EventSubscribe
+    private void migrationTips(ZKShowMigrationDataEvent event) {
+        FXUtil.runLater(() -> {
+            try {
+                StageManager.showStage(ZKMigrationTipsController.class, StageManager.getPrimaryStage());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                MessageBox.exception(ex, I18nHelper.operationException());
+            }
+        });
+    }
 }

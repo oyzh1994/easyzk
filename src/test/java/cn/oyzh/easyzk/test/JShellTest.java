9ec4b05bb59faef9f99c298edf10f5149b15154a
oyzh <oyzh.1994@qq.com>
2025-05-30 20:28:32 +0800

    1. jdk要求降低到21，javafx版本相应降低

--- src/test/java/cn/oyzh/easyzk/test/JShellTest.java
+++ src/test/java/cn/oyzh/easyzk/test/JShellTest.java
@@ -1,28 +1,28 @@
-package cn.oyzh.easyzk.test;
-
-import jdk.jshell.JShell;
-import org.junit.Test;
-
-import javax.swing.*;
-import java.awt.*;
-
-/**
- * @author oyzh
- * @since 2025-02-28
- */
-public class JShellTest {
-
-    @Test
-    public void test() {
-// 创建一个 Swing JFrame 来容纳 JShell
-        JFrame frame = new JFrame();
-        frame.setLayout(new BorderLayout());
-
-        // 初始化 JShell 并添加到 JFrame 中
-        JShell jshell = JShell.builder().build();
-        frame.pack();
-
-        // 显示 Swing 界面
-        frame.setVisible(true);
-    }
-}
+//package cn.oyzh.easyzk.test;
+//
+//import jdk.jshell.JShell;
+//import org.junit.Test;
+//
+//import javax.swing.*;
+//import java.awt.*;
+//
+///**
+// * @author oyzh
+// * @since 2025-02-28
+// */
+//public class JShellTest {
+//
+//    @Test
+//    public void test() {
+//// 创建一个 Swing JFrame 来容纳 JShell
+//        JFrame frame = new JFrame();
+//        frame.setLayout(new BorderLayout());
+//
+//        // 初始化 JShell 并添加到 JFrame 中
+//        JShell jshell = JShell.builder().build();
+//        frame.pack();
+//
+//        // 显示 Swing 界面
+//        frame.setVisible(true);
+//    }
+//}
